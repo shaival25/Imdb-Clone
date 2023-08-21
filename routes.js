9861adfe36d381ba12b73fc9d9d5a1f3dd9a14b9
shaival25 <shaivaldoshi5@gmail.com>
2023-08-21 12:17:41 +0530

    1.4

--- routes.js
+++ routes.js
@@ -3,5 +3,14 @@ var userController = require('./controller/userController')
 var genreController = require('./controller/genreController')
 
 router.post('/createuser', userController.addUser)
+router.get('/', {
+    let data = {
+        name: "GFG",
+        age: 18,
+        male: true
+    }
+
+    res.send(data);
+})
 
 module.exports = router
