59734646989a260c96213c98d3302758eb4645a1
shaival25 <shaivaldoshi5@gmail.com>
2023-08-21 12:24:23 +0530

    1.4

--- routes.js
+++ routes.js
@@ -3,14 +3,15 @@ var userController = require('./controller/userController')
 var genreController = require('./controller/genreController')
 
 router.post('/createuser', userController.addUser)
-router.get('/', {
+router.get('/', (req, res) => {
+
     let data = {
         name: "GFG",
         age: 18,
         male: true
     }
 
-    res.send(data);
+    res.send(data)
 })
 
 module.exports = router
